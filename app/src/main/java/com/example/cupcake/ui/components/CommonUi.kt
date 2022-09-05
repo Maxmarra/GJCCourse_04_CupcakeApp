e0c3cefa2664e023b3bd14f15216d7cea926fa6d
Maxmarra <5919669@gmail.com>
2022-09-05 10:39:26 +0300

    starter

--- app/src/main/java/com/example/cupcake/ui/components/CommonUi.kt
+++ app/src/main/java/com/example/cupcake/ui/components/CommonUi.kt
@@ -10,7 +10,9 @@ import com.example.cupcake.R
  * Composable that displays formatted [price] that will be formatted and displayed on screen
  */
 @Composable
-fun FormattedPriceLabel(subtotal: String, modifier: Modifier = Modifier) {
+fun FormattedPriceLabel(
+    subtotal: String,
+    modifier: Modifier = Modifier) {
     Text(
         text = stringResource(R.string.subtotal_price, subtotal),
         modifier = modifier
